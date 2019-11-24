076c66905f8a598d70552eba34811d2dd8cb3c92
mistrys47 <248.deep@gmail.com>
2019-11-24 15:17:59 +0530

    serviceproviders table added

--- app/src/main/java/com/example/identity/database.java
+++ app/src/main/java/com/example/identity/database.java
@@ -13,7 +13,9 @@ class database extends SQLiteOpenHelper {
     public static final String COL2 = "value";
     public static final String COL3 = "verified";
     public static final String COL4 = "verified_by";
-
+    public static final String TABLE_NAME2 = "serviceproviders";
+    public static final String COL21 = "id";
+    public static final String COL22 = "name";
     public database(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
@@ -21,12 +23,13 @@ class database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME1 +" (field_name TEXT,value TEXT,verified TEXT,verified_by TEXT)");
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME2 +" (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME1);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME2);
         onCreate(db);
     }
 
@@ -66,6 +69,20 @@ class database extends SQLiteOpenHelper {
     public Cursor getuserdetails(SQLiteDatabase db){
         Cursor c=db.rawQuery("select * from " +TABLE_NAME1,null);
         return c;
+    }
+    public boolean insert2(SQLiteDatabase db,String name){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL22,name);
+        long x = db.insert(TABLE_NAME2,null,contentValues);
+        if(x==-1)
+            return false;
+        return true;
+    }
+    public boolean check_serviceprovider(SQLiteDatabase db,String  s){
+        Cursor cursor = db.query(TABLE_NAME2,new String[]{"name"},"name=?",new String[]{s},null,null,null  );
+        if(cursor.getCount()>0)
+            return true;
+        return false;
     }
 }
 
