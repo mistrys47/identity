b5dee04c3bb4b16c88319da4e62a15344aef13fb
mistrys47 <248.deep@gmail.com>
2020-06-13 03:18:46 -0700

    Removed unnecessary toast

--- app/src/main/java/com/example/identity/server.java
+++ app/src/main/java/com/example/identity/server.java
@@ -4,7 +4,7 @@ public class server {
     String server_name;
     public server()
     {
-        server_name="https://.ngrok.io";
+        server_name="https://c4beae8a53db.ngrok.io";
     }
     public String getServer_name() {
         return server_name;
